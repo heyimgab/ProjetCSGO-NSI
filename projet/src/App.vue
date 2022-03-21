fd7a7d2e66146820662a697d9de3ed2d77a67066
gab <gab@gvbnn.eu>
2022-03-21 13:29:26 +0100

    useless, je delete au prochain push

---
+++ projet/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <sequential-entrance>
+    <div class="box" v-for="app in apps" :key="app">{{ app }}</div>
+  </sequential-entrance>
+</template>
+
+<script>
+import projet from './components/projet.vue'
+
+export default {
+  name: 'App',
+  components: {
+    projet
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
