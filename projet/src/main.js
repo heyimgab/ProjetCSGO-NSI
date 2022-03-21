8adef4de6b4e8a79ee37ebb105c3a5414ec444ab
gab <47774056+heyimgab@users.noreply.github.com>
2022-03-21 13:51:53 +0100

    suppression des fichiers useless, natif only

--- projet/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import 'vue-sequential-entrance/vue-sequential-entrance.css'
-import VueSequentialEntrance from 'vue-sequential-entrance'
-VueSequentialEntrance.use
-
-createApp(App).mount('#app')
-
